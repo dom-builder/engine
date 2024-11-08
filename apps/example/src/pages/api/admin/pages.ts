d066dc778379889c5b6b85057f0776267d65bab1
Alex Oliveira <alexoliveira7x@gmail.com>
2024-11-08 17:36:58 +0000

    remove kv

--- apps/example/src/pages/api/admin/pages.ts
+++ apps/example/src/pages/api/admin/pages.ts
@@ -1,4 +1,3 @@
-import { kv } from "@vercel/kv";
 import type { NextApiRequest, NextApiResponse } from "next";
 import baseSchema from "../../../utils/base_schema.json";
 import baseTheme from "../../../utils/base_theme.json";
@@ -7,39 +6,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const normalize = (str: string) => str.toLowerCase().replace(/\s/g, "_");
-  const storeNameQuery = req.query.store_name;
-  const key = normalize(`${storeNameQuery}-schema`);
-
-  if (!storeNameQuery) {
-    return res.status(400).json({ error: "Store name is required" });
-  }
-
-  if (req.method === "DELETE") {
-    await kv.set(key, { schema: baseSchema, theme: baseTheme });
-  }
-
-  if (req.method === "POST") {
-    const { schema, theme } = req.body;
-
-    if (!schema || !theme) {
-      return res.status(400).json({ error: "Schema is required" });
-    }
-
-    await kv.set(key, { schema, theme });
-    return res.status(200).json({ schema, theme });
-  }
-
-  if (!storeNameQuery) {
-    return res.status(400).json({ error: "Store name is required" });
-  }
-
-  const layout = await kv.get(key);
-
-  if (layout === null) {
-    kv.set(key, { schema: baseSchema, theme: baseTheme });
-    return res.status(200).json({ schema: baseSchema, theme: baseTheme });
-  }
-
-  res.status(200).json(layout);
+  return res.status(200).json({ schema: baseSchema, theme: baseTheme });
 }
